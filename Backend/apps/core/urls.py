0e7ba6f17d5dd56283b65fe5ac04d7f67af5990a
Yamil Llampi <yamil250100@gmail.com>
2024-02-08 12:29:16 -0500

    up

--- Backend/apps/core/urls.py
+++ Backend/apps/core/urls.py
@@ -4,8 +4,5 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
-<<<<<<< HEAD
-=======
     path('recibir-datos/', views.recibir_datos, name='recibir-datos')
->>>>>>> 59e2228543db0f0dcb673995130f82cbce05b492
 ]
