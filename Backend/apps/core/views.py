01a2c8a5c408e2c7ecb3026daab1aee14d77e328
Yamil Llampi <yamil250100@gmail.com>
2024-02-08 23:04:27 -0500

    add size product

--- Backend/apps/core/views.py
+++ Backend/apps/core/views.py
@@ -56,9 +56,10 @@ def get_product(request):
                 'id': product.id,
                 'name_product': product.name_product,
                 'price_product': f'{product.price_product}',
+                'size_product': f'{product.size_product}',
                 'type_product': f'{product.type_product}',
                 'rating': f'{product.rating}',
-                'dominant_color': f'{product.dominant_color}'
+                'dominant_color': f'{product.dominant_color}',
                 'stock': product.stock,
                 'image_url': f'http://localhost:8000{product.image_product.url}'
             }
