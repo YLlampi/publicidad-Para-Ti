33c28144b26646cd231928961069327b13161bdf
Yamil Llampi <yamil250100@gmail.com>
2024-02-08 23:22:10 -0500

    add path api

--- Backend/apps/core/urls.py
+++ Backend/apps/core/urls.py
@@ -5,5 +5,7 @@ app_name = 'core'
 
 urlpatterns = [
     path('recibir-datos/', views.recibir_datos, name='recibir-datos'),
-    path('get-product/', views.get_product, name='get-product')
+    path('get-product/', views.get_product, name='get-product'),
+    path('get-product-by-emotion/', views.get_products_by_emotion,
+         name='get-product-by-emotion')
 ]
